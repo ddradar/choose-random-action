79ca1a034639e0c5f121b3255eefe3973b9142bb
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-09-04 09:04:41 +0900

    chore(eslint): use @vitest/eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 /* eslint-disable @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access */
 import eslint from '@eslint/js'
+import vitest from '@vitest/eslint-plugin'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import vitest from 'eslint-plugin-vitest'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -18,10 +18,33 @@ export default tseslint.config(
     }
   },
   // Prettier
-  // @ts-expect-error - types are incorrect
   eslintConfigPrettier,
   // vitest
-  { ...vitest.configs.recommended, files: ['test/**'] },
+  {
+    files: ['**/*.test.ts'],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      'vitest/consistent-test-filename': 'error',
+      'vitest/consistent-test-it': ['error', { fn: 'test' }],
+      'vitest/no-alias-methods': 'error',
+      'vitest/no-conditional-in-test': 'error',
+      'vitest/no-disabled-tests': 'warn',
+      'vitest/no-duplicate-hooks': 'error',
+      'vitest/no-focused-tests': 'error',
+      'vitest/no-import-node-test': 'error',
+      'vitest/no-standalone-expect': 'error',
+      'vitest/no-test-return-statement': 'error',
+      'vitest/prefer-comparison-matcher': 'error',
+      'vitest/prefer-each': 'error',
+      'vitest/prefer-equality-matcher': 'error',
+      'vitest/prefer-hooks-in-order': 'error',
+      'vitest/prefer-hooks-on-top': 'error',
+      'vitest/prefer-mock-promise-shorthand': 'error',
+      'vitest/prefer-strict-equal': 'error',
+      'vitest/require-to-throw-message': 'error'
+    }
+  },
   // simple-import-sort
   {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
